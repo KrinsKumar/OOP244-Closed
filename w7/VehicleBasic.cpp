a05e42c9ba1f8d826dd0025270160558d8384edf
Krins <krinskumar@gmail.com>
2022-11-10 13:07:40 -0500

    vehicalModule

--- w7/VehicleBasic.cpp
+++ w7/VehicleBasic.cpp
@@ -11,10 +11,55 @@ Section:
 #include <cstring>
 #include "VehicleBasic.h"
 
+using namespace std;
+
 namespace sdds
 {
-	
-	
-	
-	
+    VehicalBasic::VehicalBasic(const char licenseNumber[], const int buildYear) {
+        strcpy(m_licenseNumber, licenseNumber);
+        m_buildYear = buildYear;
+        strcpy(m_address, "Factory");
+    }
+
+    void VehicalBasic::NewAddress(const char* address) {
+        cout << "|";
+        cout.fill(' ');
+        cout.width(8);
+        cout.setf(ios::right);
+        cout << m_licenseNumber << "| |";
+        cout.width(20);
+        cout << m_address <<" --->";
+        cout.unsetf(ios::right);
+        cout.setf(ios::left);
+        cout.width(20);
+        cout << address << " | " << endl;
+        cout.unsetf(ios::left);
+
+
+        strcpy(m_address, address);
+    }
+
+    std::ostream& VehicalBasic::write(std::ostream& os) const {
+       os << "| " << m_buildYear << "| " << m_licenseNumber << "| " << m_address;
+       return os;
+    }
+
+    std::istream& VehicalBasic::read(std::istream& is) {
+        cout << "Built year: ";
+        is >> m_buildYear;
+        cout << "License plate: ";
+        is >> m_licenseNumber;
+        cout << "Current location: ";
+        is >> m_address;
+        return is;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const VehicalBasic& printVehical){
+        return printVehical.write(os);
+    }
+
+    std::istream& operator>>(std::istream& is, VehicalBasic& readVehical) {
+        return readVehical.read(is);
+    }
+
 }
--- w7/VehicleBasic.h
+++ w7/VehicleBasic.h
@@ -12,7 +12,21 @@ Section:
 namespace sdds
 {
 
+    class VehicalBasic {
+        char m_licenseNumber[9];
+        char m_address[64];
+        int m_buildYear;
 
+        public:
+            VehicalBasic(const char licenseNumber[], const int buildYear);
+            void NewAddress(const char* address);
+            std::ostream& write(std::ostream& os) const;
+            std::istream & read(std::istream & is);
+
+    };
+
+    std::ostream& operator<<(std::ostream& os, const VehicalBasic& printVehical);
+    std::istream& operator>>(std::istream& is, VehicalBasic& readVehical);
 
 }
 
