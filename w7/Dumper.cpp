6312099ddb6691b499a0d654abb7c9006ad1bebf
Krins <krinskumar@gmail.com>
2022-11-10 14:06:04 -0500

    Dumper-proto

--- w7/Dumper.cpp
+++ w7/Dumper.cpp
@@ -9,11 +9,58 @@ Section:
 #include "Dumper.h"
 #include "VehicleBasic.h"
 
+using namespace std;
 namespace sdds
 {
+    Dumper::Dumper(const char* licenseNumber, int buildYear, float capacity, const char* address)
+    :VehicleBasic(licenseNumber, buildYear) {
+        NewAddress(address);
+        m_currentLoad = 0;
+        m_maxLoadWeight = capacity;
+    }
+
+    bool Dumper::loaddCargo(double cargo) {
+        bool load = false;
+        if (cargo + m_currentLoad < m_maxLoadWeight) {
+            load = true;
+            m_currentLoad = cargo + m_currentLoad;
+        }
+        return load;
+    }
+
+    bool Dumper::unloadCargo() {
+        bool unload = false;
+        if (m_currentLoad != 0) {
+            unload = true;
+            m_currentLoad = 0;
+        }
+        return unload;
+    }
+
+    std::ostream& Dumper::write(std::ostream& os) const {
+        VehicleBasic::write(os);
+        os << " | " << m_currentLoad << "/" << m_maxLoadWeight;
+        return os;
+    }
+
+    std::istream& Dumper::read(std::istream& is) {
+        cout << "Built year: ";
+        cin << updateYear();
+        cout << "License plate: ";
+        is >> m_licenseNumber;
+        cout << "Current location: ";
+        is >> m_address;
+
+        return is;
+    }
 	
-	
-	
-	
+    std::ostream& operator<<(std::ostream& os, const Dumper& printDumper) {
+        return printDumper.write(os);
+    }
+
+    std::istream& operator>>(std::istream& is, Dumper& readDumper) {
+        return readDumper.read(is);
+    }
+
 }
 
--- w7/Dumper.h
+++ w7/Dumper.h
@@ -12,10 +12,22 @@ Section:
 
 namespace sdds
 {
-	
-	
-	
-	
+
+    class Dumper :public VehicleBasic {
+        float m_maxLoadWeight;
+        float m_currentLoad;
+
+        public: 
+            Dumper(const char *licenseNumber, int buildYear, float capacity, const char* address);
+            bool loaddCargo(double cargo);
+            bool unloadCargo();
+            std::ostream& write(std::ostream& os) const;
+            std::istream& read(std::istream& is);
+    };
+
+    std::ostream& operator<<(std::ostream& os, const Dumper& printDumper);
+    std::istream& operator>>(std::istream& is, Dumper& readDumper);
+
 }
 #endif
 
